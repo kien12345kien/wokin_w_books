dec9033a1537ac38230a21d036f263970a279bda
kien12345kien <48080802+kien12345kien@users.noreply.github.com>
2024-09-13 15:31:29 +0700

    Update checkCmdInPath.sh

--- BOOK/bash/wicked/1_missing_code_lib/checkCmdInPath.sh
+++ BOOK/bash/wicked/1_missing_code_lib/checkCmdInPath.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
 in_path()
+{
+	cmd=$1	ourpath=$2	result=1
+	oldIfs=$IFS	IFS=":"
+
+	for directory in "$ourpath"
+	do
+		if [ -x $directory/$cmd ] ;then
+			result=0
+		fi
+	done
+	IFS-$oldIFS
+	return $result
+}
+
+checkForCmdInPath(){
+	var=$1
+
+	if ["$var" != ""]; then
+		if["${var:0:1}" = "/" ]; then
+			if[! -x $var ]; then
+				return1
+			fi
+		elif ! inpath$var "$PATH"; then
+			return 2
+		fi
+	fi
+}
